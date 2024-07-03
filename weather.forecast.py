d32ced3f898a81e3afd024e529814885a1abbbc5
Zander2895 <jacquesalexander71@gmail.com>
2024-07-03 17:46:54 -0400

    first commit

---
+++ weather.forecast.py
@@ -0,0 +1,6 @@
+#Task 1
+Fahrenheit = input("What is the temperature in Fahrenheit that you would like converted? ")
+
+#Task 2
+Celsius = (Fahrenheit - 32 * 5/9)
+print("Temperature in celsius: ", Celsius)
